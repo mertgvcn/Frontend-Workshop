c945ae0f0d44a4fe356b85e01396e7cfe775e908
mert_gvcn <mertguvercin2942@gmail.com>
2023-06-17 01:07:43 +0300

    js output types

---
+++ 2_Javascript/a_outputs/outputs.js
@@ -0,0 +1,9 @@
+//document.write ile ekrana çıktı verme
+document.write("İlk ders " + (5+2));
+
+//console.log ile konsolo çıktı verme
+console.log("5. satırda hata var! " + (4/2));
+console.clear();
+
+//alert ile uyarı poplatma
+alert("9.satırda hata var olabilir " + (3*3));
