add907df8cd261b3a650102648da319bced6ad5e
mert_gvcn <mertguvercin2942@gmail.com>
2023-06-24 01:51:51 +0300

    set - data structure

---
+++ 2_Javascript/iii_EcmaScript (ES6)/f_set/set.js
@@ -0,0 +1,15 @@
+//? SET - Data Structure (Aynı değerden bir tane bulunabilir.)
+
+/*
+    set.add(value) //her türden olabilir.
+    set.size;
+    set.delete(value)
+    set.has(value)
+
+*/
+
+const set = new Set();
+
+//önceki dersteki gibi for-of, foreach ile değerleri gezebiliriz
+//arrayden sete, setten arraye geçebiliriz. 
+
